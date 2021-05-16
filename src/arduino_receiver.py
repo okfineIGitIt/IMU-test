47bb6c2d3c4699821b88a3f5e56472ec22cf04e5
Rehann J <rehannj@gmail.com>
2021-05-16 19:12:34 -0400

    Basic tkinter matplotlib plotting: Can now plot data in a new window while receiving data from com port.

--- src/arduino_receiver.py
+++ src/arduino_receiver.py
@@ -2,6 +2,9 @@ import time
 
 import serial
 
+from data_plotting import IMUPlot
+from utils import data_format_conversions as dfc
+
 ARDUINO_COM_PORT = "COM3"
 BAUD_RATE = 115200
 
@@ -28,8 +31,22 @@ def flush_io_buffers(serial_conn: serial.Serial):
 
 if __name__ == "__main__":
     ser = serial.Serial(ARDUINO_COM_PORT, baudrate=BAUD_RATE, dsrdtr=False)
+    imu_plot = IMUPlot()
 
+    i = 0
     while True:
         response = ser.read_until()
         response_str = response.decode("utf-8").rstrip("\n")
+        response_str = response_str.strip("\r")
         print(f"response_str: {response_str}")
+
+        rot_data = dfc.parse_rotation_string_to_dict(response_str)
+
+        if rot_data is None:
+            continue
+
+        rot_x = rot_data["X"]
+        imu_plot.add_data_point(i, rot_x)
+        i = i + 1
+
+
---
+++ src/data_plotting.py
@@ -0,0 +1,66 @@
+import tkinter
+
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
+# style.use('seaborn-whitegrid')
+
+root = tkinter.Tk()
+root.wm_title("Embedding in Tk")
+
+fig = Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot(111)
+
+
+class IMUPlot:
+    def __init__(self):
+        self.x = []
+        self.y = []
+
+        # making figure with data
+        self.fig = fig
+        self.ax = ax
+
+        # make tkinter plot
+        self.canvas = FigureCanvasTkAgg(self.fig, master=root)  # A tk.DrawingArea.
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        self.toolbar = NavigationToolbar2Tk(self.canvas, root)
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        def on_key_press(event):
+            print("you pressed {}".format(event.key))
+            key_press_handler(event, self.canvas, self.toolbar)
+
+        def _quit():
+            # root.quit()  # stops mainloop
+            root.destroy()
+
+        self.canvas.mpl_connect("key_press_event", on_key_press)
+
+        button = tkinter.Button(master=root, text="Quit", command=_quit)
+        button.pack(side=tkinter.BOTTOM)
+
+        self._update_tkinter_app()
+
+    @staticmethod
+    def _update_tkinter_app():
+        root.update_idletasks()
+        root.update()
+
+    def add_data_point(self, x, y):
+        self.ax.clear()
+        self.x.append(x)
+        self.y.append(y)
+        self.ax.plot(self.x, self.y)
+        self.canvas.draw()
+
+        self._update_tkinter_app()
+
+
+# tkinter.mainloop()
