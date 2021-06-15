1c8afbfcbe5511aafaf8e52691eb890adf1c34a1
Rehann J <rehannj@gmail.com>
2021-06-14 21:44:43 -0400

    Added some skeleton classes for Frames and their contents

--- src/gui_layout.py
+++ src/gui_layout.py
@@ -1,49 +1,58 @@
 import tkinter as tk
 
-window = tk.Tk()
+
+class ControlFrame:
+    """Frame with interactive elements to control aspects of the GUI"""
+
+    pass
 
 
-render_frame = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1
-)
-render_label = tk.Label(master=render_frame, text=f"Row {1}\nColumn {1}")
+class GraphFrame:
+    """Frame with graph displaying IMU readings"""
 
-graph_frame = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1
-)
-graph_label = tk.Label(master=graph_frame, text=f"Row {1}\nColumn {2}")
+    pass
 
-control_frame = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1
-)
-control_label = tk.Label(master=control_frame, text=f"Row {2}\nColumn {1}")
 
-monitor_frame = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1
-)
-monitor_label = tk.Label(master=monitor_frame, text=f"Row {2}\nColumn {2}")
+class RenderFrame:
+    """Frame with 3d render if current IMU orientation"""
 
-window.columnconfigure(1, weight=1, minsize=75)
-window.rowconfigure(1, weight=1, minsize=50)
-window.columnconfigure(2, weight=1, minsize=75)
-window.rowconfigure(2, weight=1, minsize=50)
+    pass
 
-render_label.pack(padx=30, pady=30)
-graph_label.pack(padx=30, pady=30)
-control_label.pack(padx=30, pady=30)
-monitor_label.pack(padx=30, pady=30)
 
-render_frame.grid(row=1, column=1)
-graph_frame.grid(row=1, column=2)
-control_frame.grid(row=2, column=1)
-monitor_frame.grid(row=2, column=2)
+class MonitorFrame:
+    """Frame with serial monitor output from IMU"""
 
-window.mainloop()
+    pass
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+
+    render_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+    render_label = tk.Label(master=render_frame, text=f"Row {1}\nColumn {1}")
+
+    graph_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+    graph_label = tk.Label(master=graph_frame, text=f"Row {1}\nColumn {2}")
+
+    control_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+    control_label = tk.Label(master=control_frame, text=f"Row {2}\nColumn {1}")
+
+    monitor_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+    monitor_label = tk.Label(master=monitor_frame, text=f"Row {2}\nColumn {2}")
+
+    window.columnconfigure(1, weight=1, minsize=75)
+    window.rowconfigure(1, weight=1, minsize=50)
+    window.columnconfigure(2, weight=1, minsize=75)
+    window.rowconfigure(2, weight=1, minsize=50)
+
+    render_label.pack(padx=30, pady=30)
+    graph_label.pack(padx=30, pady=30)
+    control_label.pack(padx=30, pady=30)
+    monitor_label.pack(padx=30, pady=30)
+
+    render_frame.grid(row=1, column=1)
+    graph_frame.grid(row=1, column=2)
+    control_frame.grid(row=2, column=1)
+    monitor_frame.grid(row=2, column=2)
+
+    window.mainloop()
