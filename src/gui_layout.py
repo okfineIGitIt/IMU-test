98cab7619f7040161f3b92385e93207ddc674162
Rehann J <rehannj@gmail.com>
2021-06-14 22:34:04 -0400

    Added content to skeleton classes, basic instances of graph, canvas, control panel and serial monitor text edit.

--- src/gui_layout.py
+++ src/gui_layout.py
@@ -1,58 +1,108 @@
 import tkinter as tk
 
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
 
 class ControlFrame:
     """Frame with interactive elements to control aspects of the GUI"""
 
-    pass
+    def __init__(self, frame):
+        self.frame = frame
+        self.configure_ui()
+
+    def configure_ui(self):
+        button = tk.Button(self.frame, text="Please No")
+        button1 = tk.Button(self.frame, text="Please Yes")
+
+        button.pack()
+        button1.pack()
 
 
 class GraphFrame:
     """Frame with graph displaying IMU readings"""
 
-    pass
+    def __init__(self, frame, window):
+        # self.x = []
+        # self.y = []
+        self.frame = frame
+        self.window = window
+
+        # making figure with data
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+
+        # make tkinter plot
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)  # A tk.DrawingArea.
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame)
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        def on_key_press(event):
+            print("you pressed {}".format(event.key))
+            key_press_handler(event, self.canvas, self.toolbar)
+
+        self.canvas.mpl_connect("key_press_event", on_key_press)
+
+        self._update_tkinter_app()
+
+    def _update_tkinter_app(self):
+        self.window.update_idletasks()
+        self.window.update()
 
 
 class RenderFrame:
     """Frame with 3d render if current IMU orientation"""
 
-    pass
+    def __init__(self, frame):
+        self.frame = frame
+        self.configure_ui()
+
+    def configure_ui(self):
+        canvas = tk.Canvas(self.frame)
+
+        canvas.pack()
 
 
 class MonitorFrame:
     """Frame with serial monitor output from IMU"""
 
-    pass
+    def __init__(self, frame):
+        self.frame = frame
+        self.configure_ui()
+
+    def configure_ui(self):
+        text_editor = tk.Text(self.frame)
+
+        text_editor.pack()
 
 
 if __name__ == "__main__":
     window = tk.Tk()
 
     render_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-    render_label = tk.Label(master=render_frame, text=f"Row {1}\nColumn {1}")
-
     graph_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-    graph_label = tk.Label(master=graph_frame, text=f"Row {1}\nColumn {2}")
-
     control_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-    control_label = tk.Label(master=control_frame, text=f"Row {2}\nColumn {1}")
-
     monitor_frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-    monitor_label = tk.Label(master=monitor_frame, text=f"Row {2}\nColumn {2}")
+
+    render_frame.grid(row=1, column=1)
+    graph_frame.grid(row=1, column=2)
+    control_frame.grid(row=2, column=1)
+    monitor_frame.grid(row=2, column=2)
 
     window.columnconfigure(1, weight=1, minsize=75)
     window.rowconfigure(1, weight=1, minsize=50)
     window.columnconfigure(2, weight=1, minsize=75)
     window.rowconfigure(2, weight=1, minsize=50)
 
-    render_label.pack(padx=30, pady=30)
-    graph_label.pack(padx=30, pady=30)
-    control_label.pack(padx=30, pady=30)
-    monitor_label.pack(padx=30, pady=30)
-
-    render_frame.grid(row=1, column=1)
-    graph_frame.grid(row=1, column=2)
-    control_frame.grid(row=2, column=1)
-    monitor_frame.grid(row=2, column=2)
+    control = ControlFrame(frame=control_frame)
+    monitor = MonitorFrame(frame=monitor_frame)
+    render = RenderFrame(frame=render_frame)
+    graph = GraphFrame(frame=graph_frame, window=window)
 
     window.mainloop()
